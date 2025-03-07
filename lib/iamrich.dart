1bf7884129bdc77b27327bf77bcb08977fdade8e
haint <ggrreenn003@gmail.com>
2025-03-07 14:48:54 +0700

    start

---
+++ lib/iamrich.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class IamRich extends StatelessWidget{
+  const IamRich({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            backgroundColor: Colors.blueGrey,
+            appBar: AppBar(
+              title: Text("I Am Rich"),
+              backgroundColor: Colors.white,
+            ),
+            body: Center(
+              child: Image(image: AssetImage('images/diamond.png')),
+            )
+        )
+    );
+  }
+  
+}
---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:start/iamrich.dart';
+// cd android
+// .\gradlew clean build
+void main() {
+  runApp(const IamRich());
+}
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: const Text("Hello Hello"),
+//         ),
+//         body: const Row(
+//           crossAxisAlignment: CrossAxisAlignment.start,
+//           children: <Widget>[
+//             CircleAvatar(
+//               radius: 35,
+//               backgroundImage: NetworkImage(
+//                 'https://avatars.githubusercontent.com/u/33576285?v=4',
+//               ),
+//             ),
+//             Padding(padding: EdgeInsets.all(2.0)),
+//             Column(
+//               children: <Widget>[
+//                 Text(
+//                   'Thanh Hai',
+//                   style: TextStyle(
+//                     fontSize: 20,
+//                     fontWeight: FontWeight.bold,
+//                   ),
+//                 ),
+//                 Text(
+//                   'Learning Flutter',
+//                   style: TextStyle(
+//                     fontSize: 15,
+//                   ),
+//                 )
+//               ],
+//             )
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
